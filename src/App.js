d7b9c88d6b6f9157f9b3f7f9ec9c2edbc40a7cc0
BangtanAi <kairova-94@mail.ru>
2021-01-31 00:46:06 +0600

    40. Textarea, button in Dialogs/dialogspage

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ const App = (props) => {
                 <Navbar />
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
-                           render={ () => <Dialogs
-                               dialogsPage={props.state.dialogsPage} /> }/>
+                           render={ () => <Dialogs store = {props.store} /> } />
 
                     <Route path='/profile'
                            render={ () => <Profile
@@ -27,40 +26,5 @@ const App = (props) => {
 
 export default App;
 
-
-// import React, { Component } from 'react';
-// import './App.css';
-// import Dialogs from './components/Dialogs/Dialogs';
-// import Header from './components/Header/Header';
-// import Navbar from './components/Navbar/Navbar';
-// import News from './components/News/News';
-// import Profile from './components/Profile/Profile';
-// import Settings from './components/Settings/Setting';
-// import { BrowserRouter, Route } from 'react-router-dom';
-
-
-
-// const App = (props) => {
-
-//   return (
-//     <BrowserRouter>
-//     <div className = 'app-wrapper'>
-//       <Header/ >
-//       <Navbar/ >
-//       <div className = 'app-wrapper-content'>
-//         <Route path='/Dialogs' 
-//           render={() => <Dialogs state={props.state.dialogsPage} /> }/>
-//         <Route path='/Profile'
-//           render ={()=> <Profile state={props.state.profilePage}
-//                                  addPost={props.addPost}/> } />
-//         <Route path='/News' render={()=> <News />}/>
-//         <Route path='/Settings' render={()=> <Settings />}/>
-//       </div>
-//     </div>
-    
-//     </BrowserRouter>
-//   );
-// }
-// export default App;
 
 
