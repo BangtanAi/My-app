ac5d060d9435b2e9c9acfdc056215b19a25bf0a6
BangtanAi <kairova-94@mail.ru>
2021-01-31 16:00:53 +0600

    41. Вынесла action creator, action type, функции в отдельные файлы редьюсеры REDUCER

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-import {sendMessageCreator, updateNewMessageBodyCreator} from './../../redux/state';
+import { sendMessageCreator, updateNewMessageBodyCreator } from './../../redux/dialogs-reducer';
 
 const Dialogs = (props) => {
-    debugger;
    let state = props.store.getState().dialogsPage;
    let dialogsElements = state.dialogs.map( d => <DialogItem name={d.name} id={d.id} />  );
    let messagesElements = state.messages.map( m => <Message message={m.message}/> );
