9b519c421edaced5c66ec9c607e0026e6622ace2
BangtanAi <kairova-94@mail.ru>
2021-02-07 20:49:49 +0600

    54. жизненный цикл, life cycle methods, componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,14 +4,11 @@ import styles from './users.module.css';
 import userPhoto from '../../assets/images/user-image.png';
 
 class Users extends React.Component {
-
-  constructor(props){
-    super(props);
-    alert('NEW');
+  componentDidMount(){
     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
         this.props.setUsers(response.data.items);
       });
-}
+  }
 
   render() {
     return <div>
