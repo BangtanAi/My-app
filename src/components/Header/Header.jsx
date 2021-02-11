d330f2d3680d57d6bd18b7ad7d5e977acca9bc5e
BangtanAi <kairova-94@mail.ru>
2021-02-12 00:43:39 +0600

    61. cookie, login, auth/me, sign in with name Aliya

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
         <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+
+        <div className={s.loginBlock}>
+            { props.isAuth ? props.login
+            : <NavLink to={'/login'}>Login</NavLink> }
+        </div>
     </header>
 }
 
 export default Header;
 
-// import React from 'react';
-// import s from './Header.module.css';
-
-// const Header = () => {
-//     return (
-//         <header className = {s.header}>
-//         <img src='https://lh3.googleusercontent.com/Ra0keqq9FtFgPMU0GU6SE8HiHnnjQRkQWhMAN14JFT4tBZrG_8DyJJ1EiWId4-a-_9c'></img>
-//       </header>
-//     )
-// }
-
-// export default Header;
