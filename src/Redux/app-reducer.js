4faf6f730a151ebbe0253506e5fd707ca90241af
BangtanAi <kairova-94@mail.ru>
2021-03-11 17:41:59 +0600

    81. reselect. Селекторы 1  часть

--- src/Redux/app-reducer.js
+++ src/Redux/app-reducer.js
@@ -22,9 +22,11 @@ const appReducer = (state = initialState, action) => {
 export const initializedSuccess = () => ({type: INITIALIZED_SUCCESS})
 
 export const initializeApp = () => (dispatch) => {
-    let dispatchResult = dispatch(getAuthUserData());
-    dispatch(initializedSuccess());
-    
+    let promise = dispatch(getAuthUserData());
+    Promise.all([promise])
+        .then(() => {
+            dispatch(initializedSuccess());
+        });     
 }
 
 
