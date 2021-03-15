d21f84b010eb6c91732e115b28123086d8078387
BangtanAi <kairova-94@mail.ru>
2021-03-15 19:51:20 +0600

    86. shouldComponentUpdate, PureComponent for classes, memo for functional component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,8 +5,8 @@ import { Field, reduxForm } from 'redux-form';
 import { maxLengthCreator, required } from '../../../utils/validators/validator';
 import { Textarea } from '../../common/FormsControls/FormsControl';
 
-const MyPosts = (props) => {
-  
+const MyPosts = React.memo(props => {
+    console.log('RENDERRR');
     let postsElements =
         props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>);
 
@@ -25,7 +25,7 @@ const MyPosts = (props) => {
             </div>
         </div>
     )
-}
+})
 const maxLength10 = maxLengthCreator(10);
 const AddNewPostForm = (props) => {
     return (
